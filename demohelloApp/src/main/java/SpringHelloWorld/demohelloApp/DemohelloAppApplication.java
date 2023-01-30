1147b65e8a909e739a4277aae532ed696978444e
prathyushapalisetti <prathyusha2001sai@gmail.com>
2023-01-30 22:04:17 +0530

    added

---
+++ demohelloApp/src/main/java/SpringHelloWorld/demohelloApp/DemohelloAppApplication.java
@@ -0,0 +1,13 @@
+package SpringHelloWorld.demohelloApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemohelloAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemohelloAppApplication.class, args);
+	}
+
+}
---
+++ demohelloApp/src/test/java/SpringHelloWorld/demohelloApp/DemohelloAppApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringHelloWorld.demohelloApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemohelloAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
